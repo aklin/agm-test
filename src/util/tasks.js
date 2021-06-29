b90c6fe62bcb265715fb26f7795ffeb52806d4cc
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-29 03:54:54 +0100

    define task durations

--- src/util/tasks.js
+++ src/util/tasks.js
@@ -2,10 +2,15 @@ export const Tasks = {
 	PREPARE_ORDER: 'PREPARE_ORDER',
 	SERVE_ORDER: 'SERVE_ORDER',
 	BREAK: 'BREAK',
-}
+};
 
 export const TaskDescription = {
-	[Tasks.PREPARE_ORDER]: "Prepare order for",
-	[Tasks.SERVE_ORDER]: "Serve order to",
-	[Tasks.BREAK]: "Take a break",
-}
+	[Tasks.PREPARE_ORDER]: 'Prepare order for',
+	[Tasks.SERVE_ORDER]: 'Serve order to',
+	[Tasks.BREAK]: 'Take a break',
+};
+
+export const TaskDurationMinutes = {
+	[Tasks.PREPARE_ORDER]: 2.5,
+	[Tasks.SERVE_ORDER]: 1,
+};
