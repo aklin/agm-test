b06fd44fa89955b50e62abbef0704e3cf0818960
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-29 03:53:06 +0100

    Add start/end break

--- src/components/CreateOrder/index.jsx
+++ src/components/CreateOrder/index.jsx
@@ -1,4 +1,4 @@
-import {Button, makeStyles, Paper, Typography} from "@material-ui/core";
+import {Button, Grid, makeStyles, Paper, Typography} from "@material-ui/core";
 import InputField from "../InputField";
 import {useState} from "react";
 import {Actions} from "../../hooks";
@@ -11,12 +11,15 @@ const useStyles = makeStyles({
 	},
 });
 
-export default function CreateOrder({dispatch}) {
+export default function CreateOrder({state, dispatch}) {
 	const classes = useStyles();
 
 	const [form, setForm] = useState({});
-
 	const {customerName} = form;
+
+	const {onBreak} = state;
+
+	console.log(onBreak)
 
 	return (
 		<div className={classes.topMargin}>
@@ -24,22 +27,38 @@ export default function CreateOrder({dispatch}) {
 				Incoming Order Form
 			</Typography>
 			<Paper>
-				<InputField
-					id={"customerName"}
-					data-testid={"customerName"}
-					value={customerName}
-					label={"Customer name"}
-					helperText={"Will appear on your Orders table below"}
-					onChange={(e) => {
-						e.preventDefault();
-						setForm({...form, [e.target.id]: e.target.value})
-					}}
-				/>
-				<Button onClick={(e => {
-					e.preventDefault();
-					dispatch({type: Actions.ADD_NEW_ORDER, data: form})
-					setForm({})
-				})}>Create order</Button>
+				<Grid container>
+
+					<Grid item md={4}>
+						<InputField
+							id={"customerName"}
+							data-testid={"customerName"}
+							value={customerName}
+							label={"Customer name"}
+							helperText={"Will appear on your Orders table below"}
+							onChange={(e) => {
+								e.preventDefault();
+								setForm({...form, [e.target.id]: e.target.value})
+							}}
+						/>
+						<Button onClick={(e => {
+							e.preventDefault();
+							dispatch({type: Actions.ADD_NEW_ORDER, data: form})
+							setForm({})
+						})}>Create order</Button>
+
+					</Grid>
+					<Grid item md={4}>
+						<Button data-testid={"breakBtn"}
+										onClick={(e) => {
+											e.preventDefault();
+
+											dispatch({type: onBreak ? Actions.END_BREAK : Actions.START_BREAK})
+										}}
+						>{onBreak ? 'End Break' : "Start Break"}</Button>
+					</Grid>
+
+				</Grid>
 			</Paper>
 		</div>
 	)
