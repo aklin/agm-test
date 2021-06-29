85be1ff8cda01891c1f699b7b47c17613f5d2c83
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-29 03:54:28 +0100

    add start/end break btn

--- src/components/CreateOrder/index.jsx
+++ src/components/CreateOrder/index.jsx
@@ -1,65 +1,68 @@
-import {Button, Grid, makeStyles, Paper, Typography} from "@material-ui/core";
-import InputField from "../InputField";
-import {useState} from "react";
-import {Actions} from "../../hooks";
-
+import { Button, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import InputField from '../InputField';
+import { useState } from 'react';
+import { Actions } from '../../hooks';
 
 const useStyles = makeStyles({
 	topMargin: {
-		marginTop: "50px",
-		marginBottom: "50px",
+		marginTop: '50px',
+		marginBottom: '50px',
 	},
 });
 
-export default function CreateOrder({state, dispatch}) {
+export default function CreateOrder({ state, dispatch }) {
 	const classes = useStyles();
 
 	const [form, setForm] = useState({});
-	const {customerName} = form;
+	const { customerName } = form;
 
-	const {onBreak} = state;
+	const { onBreak } = state;
 
-	console.log(onBreak)
+	console.log(onBreak);
 
 	return (
 		<div className={classes.topMargin}>
-			<Typography variant={"h4"}>
-				Incoming Order Form
-			</Typography>
+			<Typography variant={'h4'}>Incoming Order Form</Typography>
 			<Paper>
 				<Grid container>
-
 					<Grid item md={4}>
 						<InputField
-							id={"customerName"}
-							data-testid={"customerName"}
+							id={'customerName'}
+							data-testid={'customerName'}
 							value={customerName}
-							label={"Customer name"}
-							helperText={"Will appear on your Orders table below"}
+							label={'Customer name'}
+							helperText={'Will appear on your Orders table below'}
 							onChange={(e) => {
 								e.preventDefault();
-								setForm({...form, [e.target.id]: e.target.value})
+								setForm({ ...form, [e.target.id]: e.target.value });
 							}}
 						/>
-						<Button onClick={(e => {
-							e.preventDefault();
-							dispatch({type: Actions.ADD_NEW_ORDER, data: form})
-							setForm({})
-						})}>Create order</Button>
-
+						<Button
+							onClick={(e) => {
+								e.preventDefault();
+								dispatch({ type: Actions.ADD_NEW_ORDER, data: form });
+								setForm({});
+							}}
+						>
+							Create order
+						</Button>
 					</Grid>
 					<Grid item md={4}>
-						<Button data-testid={"breakBtn"}
-										onClick={(e) => {
-											e.preventDefault();
+						<Button
+							data-testid={'breakBtn'}
+							onClick={(e) => {
+								e.preventDefault();
 
-											dispatch({type: onBreak ? Actions.END_BREAK : Actions.START_BREAK})
-										}}
-						>{onBreak ? 'End Break' : "Start Break"}</Button>
+								dispatch({
+									type: onBreak ? Actions.END_BREAK : Actions.START_BREAK,
+								});
+							}}
+						>
+							{onBreak ? 'End Break' : 'Start Break'}
+						</Button>
 					</Grid>
-
 				</Grid>
 			</Paper>
 		</div>
-	)
+	);
 }
